e55b3a3540f2636d5e305161c2ea9046f408f0ad
Scott Evans <git@scott.ee>
2015-04-10 13:22:12 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,198 @@
+/**
+ * Settings
+ *
+ * Setup your project paths and requirements here
+ */
+var settings = {
+
+	// path to JS - no tasks yet, workflow in progress
+	// scriptspath: 'app/**/*.js',
+
+	// path to main scss file
+	scss: 'src/scss/style.scss',
+
+	// path to output css file
+	css: 'assets/css/style.css',
+
+	// path to watch for changed scss files
+	scsswatch: 'src/scss/**/*.scss',
+
+	// path to output css folder
+	csspath: 'assets/css/',
+
+	// path to images
+	imagespath: 'assets/images/',
+
+	// path to html
+	htmlpath: './',
+	// partials: ['app/**/*.html', '!app/index.html'],
+
+	// enable the static file server and browsersync
+	// check for unused styles in static html? - seems buggy, requires html
+	staticserver: true,
+	checkunusedcss: false,
+
+	// enable the proxied local server for browsersync
+	// static above server must be disabled
+	proxyserver: false,
+	proxylocation: 'mysite.dev'
+
+};
+
+/**
+ * Load node modules
+ */
+var autoprefixer = require('gulp-autoprefixer'),
+	browserify = require( 'browserify' ),
+	browsersync = require('browser-sync'),
+	checkcss = require( 'gulp-check-unused-css' ),
+	csscomb = require('gulp-csscomb'),
+	filter = require('gulp-filter'),
+	gulp = require('gulp'),
+	imagemin = require('gulp-imagemin'),
+	minifycss = require('gulp-minify-css'),
+	notify = require("gulp-notify"),
+	parker = require('gulp-parker'),
+	plumber = require('gulp-plumber'),
+	sass = require('gulp-sass'),
+	reactify = require( 'reactify' ),
+	sourcemaps = require('gulp-sourcemaps'),
+	sync = require('gulp-config-sync'),
+	watch = require('gulp-watch');
+
+/**
+ * Generic error handler used by plumber
+ *
+ * Display an OS notification and sound with error message
+ */
+var onError = function(err) {
+	notify.onError({
+		title:	"Gulp",
+		message: "Error: <%= error.message %>\nLine: <%= error.lineNumber %>",
+		sound:	"Sosumi"
+	})(err);
+
+	this.emit('end');
+};
+
+/**
+ * Default Task
+ *
+ * Watch for changes and run tasks
+ */
+gulp.task('default', function() {
+
+	// browsersync and local server
+	// options: http://www.browsersync.io/docs/options/
+	if (settings.staticserver) {
+		browsersync({
+			server: settings.htmlpath
+		});
+
+		// check to see if the CSS is being used
+		if (settings.checkunusedcss) {
+			gulp.watch(settings.css, ['checkcss']);
+		}
+	}
+
+	if (settings.proxyserver) {
+		browsersync({
+			proxy: settings.proxylocation
+		});
+	}
+
+	// watch for SCSS changes
+	gulp.watch(settings.scsswatch, ['styles']);
+
+	// watch for image changes
+	gulp.watch(settings.imagespath, ['images']);
+
+});
+
+/**
+ * Stylesheet Task
+ *
+ * SCSS -> CSS
+ * Autoprefix
+ * CSSComb
+ * Sourcemaps
+ * Minify
+ * Report
+ */
+gulp.task('styles', function() {
+
+	return gulp.src(settings.scss)
+		.pipe(plumber({errorHandler: onError}))
+		.pipe(sass({
+			style: 'expanded',
+			errLogToConsole: false
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(autoprefixer('last 2 versions', 'ie 8', 'ie 9'))
+		.pipe(csscomb())
+		.pipe(sourcemaps.write('./'))
+		.pipe(minifycss())
+		.pipe(gulp.dest(settings.csspath))
+		.pipe(filter('**/*.css'))
+		.pipe(browsersync.reload({stream: true}))
+		.pipe(parker())
+		.pipe(notify({ message: 'Styles task complete' }));
+});
+
+/**
+ * Images Task
+ *
+ * Run independantly when you want to optimise image assets
+ */
+gulp.task('images', function() {
+
+	return gulp.src(settings.imagespath + '**/*.{gif,jpg,png}')
+		.pipe(plumber({errorHandler: onError}))
+		.pipe(imagemin({
+			progressive: true,
+			interlaced: true,
+			//svgoPlugins: [ {removeViewBox:false}, {removeUselessStrokeAndFill:false} ]
+		}))
+		.pipe(gulp.dest(settings.imagespath))
+		.pipe(browsersync.reload({stream: true}))
+		.pipe(notify({ message: 'Images task complete' }));
+});
+
+/**
+ * CheckCSS Task
+ *
+ * Are all our styles being used correctly?
+ */
+gulp.task('checkcss', function() {
+
+	return gulp.src([ settings.css, settings.staticlocation + '*.html' ])
+		.pipe(plumber({errorHandler: onError}))
+		.pipe(checkcss());
+});
+
+/**
+ * Sync Task
+ *
+ * Sync repository details to other manifest/config files
+ */
+gulp.task('sync', function() {
+	gulp.src(['bower.json'])
+		.pipe(sync())
+		.pipe(gulp.dest('.'));
+});
+
+
+// other ideas
+// - add bower config - folders for bower to grab assets.?. not very flexible
+// - lint scss files - https://github.com/brigade/scss-lint
+// - inline fonts & SVG images - https://npmjs.org/package/gulp-sassvg/
+// - performance testing - https://www.npmjs.com/package/gulp-bench/
+//
+// notes
+// http://ericlbarnes.com/setting-gulp-bower-bootstrap-sass-fontawesome/
+// https://github.com/jacklenox/picard-present
+// http://webstoemp.com/blog/gulp-setup/
+// https://github.com/primer/primer
+// https://github.com/Falci/falci-gulp-tasks
+// https://github.com/pwnjack/prime
+// http://paislee.io/a-healthy-gulp-setup-for-angularjs-projects/
---
+++ readme.md
@@ -0,0 +1,52 @@
+![Glug Logo](http://cloud.scott.ee/images/glug.png)
+
+# Glug
+
+* Status: ✔ Active
+* Contributors: [@scottsweb](http://twitter.com/scottsweb)
+* Description: A starting point for front end projects.
+* Author: [Scott Evans](http://scott.ee)
+* Author URI: [http://scott.ee](http://scott.ee)
+* License: GNU General Public License v2.0
+* License URI: [http://www.gnu.org/licenses/gpl-2.0.html](http://www.gnu.org/licenses/gpl-2.0.html)
+
+## About
+
+A starting point for front end projects based on Gulp. It is a young project and will be improved quickly. Glug currently features:
+
+* [Browsersync](http://www.browsersync.io/)
+* [SCSS](http://sass-lang.com/documentation/file.SASS_REFERENCE.html) compiling
+* Source Maps
+* Image compression
+* [Autoprefixer](https://github.com/postcss/autoprefixer)
+* [Parker](https://github.com/katiefenn/parker)
+* Experimenting with [Bower](http://bower.io/)
+* and more coming soon!
+
+## Installation & Setup
+
+Download or checkout this repository to your local machine. Browse to the downloaded directory in your terminal app and run: `sudo npm install` (you will need to have [node and npm](https://nodejs.org/download/) on your system).
+
+Customise the settings at the top of `gulpfile.js` to your requirements. You can then run the following tasks:
+
+```
+gulp
+
+```
+
+Which watches your SCSS and images folders for changes. May also start a local Browsersync instance and static server if the settings are setup that way
+
+
+```
+gulp checkcss
+
+```
+
+If the project is a static one with HTML assets, the HTML and CSS are compared to see if any selectors can be removed from your styles.
+
+```
+gulp sync
+
+```
+
+To copy changes made to your `package.json` to other manifest files (e.g. `bower.json`).
